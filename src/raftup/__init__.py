e9fd7c891450022db717d8e4b9e4d3b2727fbbbe
L1feiyu <yaqiwu66@gmail.com>
2025-12-22 19:47:01 -0500

    Expose internal modules via raftup package and update demo imports

--- src/raftup/__init__.py
+++ src/raftup/__init__.py
@@ -1,3 +1,10 @@
 from .version import __version__
 
-__all__ = ["__version__"]
+__all__ = [
+    "__version__",
+    "_downsample",
+    "_load_data",
+    "_metrics_two_gpr",
+    "_fsgw_utils",
+    "_recoverfull_new_new_knn",
+]
